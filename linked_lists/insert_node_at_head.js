24c898b8b56ff62f2ab949b696658b5401ba0770
euniceschoi <euniceschoi93@gmail.com>
2017-01-08 00:47:33 -0500

    learn to insert node at head

---
+++ linked_lists/insert_node_at_head.js
@@ -0,0 +1,20 @@
+/*
+  Insert Node at the beginning of a linked list
+  head pointer input could be NULL as well for empty list
+  Node is defined as
+  var Node = function(data) {
+      this.data = data;
+      this.next = null;
+  }
+*/
+
+// This is a "method-only" submission.
+// You only need to complete this method.
+
+function insert(head, data) {
+    current = head;
+    head = new Node(data);
+    head.next = current;
+
+    return head
+}
